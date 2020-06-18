fa0ae2e63315fad75c453a141f102e6c54b7452e
Ahmad Kandil <A77mad.22andil@gmail.com>
2020-06-18 02:47:12 +0200

    Chat

---
+++ crepixa/ChatGroup/ChatGroup/Hubs/Chat.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatGroup.Hubs
+{
+    public class Chat : Hub
+    {
+        public async Task Send(string nick, string message)
+        {
+            await Clients.All.SendAsync("Recieve", nick, message);
+        }
+    }
+}
