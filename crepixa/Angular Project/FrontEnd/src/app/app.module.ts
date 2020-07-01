536f107e336f137f84bf8aa325c7c43eda71d7bd
Ahmad Kandil <A77mad.22andil@gmail.com>
2020-07-01 21:58:34 +0200

    Project

--- crepixa/Angular Project/FrontEnd/src/app/app.module.ts
+++ crepixa/Angular Project/FrontEnd/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestingApiComponent } from './testing-api/testing-api.component';
-import {HttpClientModule} from '@angular/common/http' ;
+import {HttpClientModule} from '@angular/common/http';
+import { LeftHeaderComponent } from './Components/left-header/left-header.component';
+import { RightHeaderComponent } from './Components/right-header/right-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestingApiComponent
+    TestingApiComponent,
+    LeftHeaderComponent,
+    RightHeaderComponent
   ],
   imports: [
     BrowserModule,
