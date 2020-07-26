9c1c728810a2922068d435b80844635f2321f25e
Ahmad Kandil <A77mad.22andil@gmail.com>
2020-07-26 07:35:49 +0200

    Team

--- crepixa/Angular Project/FrontEnd/src/app/Components/team-header/team-header.component.ts
+++ crepixa/Angular Project/FrontEnd/src/app/Components/team-header/team-header.component.ts
@@ -9,6 +9,40 @@ export class TeamHeaderComponent implements OnInit {
 
   icon = 'assets/Images/telegram.png';
 
+  employees = [
+    {
+      'name' : 'GHADA RAMADAN',
+      'job_title' : ['Marketing Head']
+    },
+    {
+      'name' : 'AIMAN MANSOUR',
+      'job_title' : ['Co-Founder', 'Graphic Design Head']
+    },
+    {
+      'name' : 'MENNA SHETA',
+      'job_title' : ['Garaphic Designer']
+    },
+    {
+      'name' : 'AHMED KANDIL',
+      'job_title' : ['Software Engineer']
+    }
+  ];
+
+  trainee = [
+    {
+      'name' : 'GHADA RAMADAN',
+      'job_title' : ['Marketing Head']
+    },
+    {
+      'name' : 'AIMAN MANSOUR',
+      'job_title' : ['Co-Founder', 'Graphic Design Head']
+    },
+    {
+      'name' : 'MENNA SHETA',
+      'job_title' : ['Garaphic Designer']
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
