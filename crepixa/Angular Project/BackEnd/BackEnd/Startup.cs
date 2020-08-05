24a36f133acc8e8dd30c0df7a42bce845e83e431
Ahmad Kandil <A77mad.22andil@gmail.com>
2020-08-05 13:24:33 +0200

    DbContext

--- crepixa/Angular Project/BackEnd/BackEnd/Startup.cs
+++ crepixa/Angular Project/BackEnd/BackEnd/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BackEnd.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +28,12 @@ namespace BackEnd
         {
             services.AddControllers();
             services.AddCors();
+
+            services.AddDbContext<StoreContext>(x =>
+            {
+                x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
             //services.AddCors(options =>
             //{
             //    options.AddPolicy("EnableCORS", builder =>
